1327a344330e48d7dc156607c148be75b211f605
Andrei <109a@mail.ru>
2021-04-20 16:35:32 +0300

    Delete unnecessary

--- src/main/java/com/springboot/microservice/soapserver/CurrencyConversionDtoEndpoint.java
+++ src/main/java/com/springboot/microservice/soapserver/CurrencyConversionDtoEndpoint.java
@@ -3,6 +3,7 @@ package com.springboot.microservice.soapserver;
 
 
 import com.springboot.microservice.CurrencyConversionDto;
+
 import com.springboot.microservice.GetCurrencyConversionDtoRequest;
 import com.springboot.microservice.GetCurrencyConversionDtoResponse;
 import lombok.Data;
@@ -20,25 +21,28 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 public class CurrencyConversionDtoEndpoint {
     private static final String NAMESPACE_URI = "http://springboot.com/microservice";
 
-    private final CurrencyConversionDtoRepository currencyConversionDtoRepository;
-    private final RestTemplateService restTemplateService;
-
     @Autowired
     public CurrencyConversionDtoEndpoint(CurrencyConversionDtoRepository currencyConversionDtoRepository, RestTemplateService restTemplateService) {
         this.currencyConversionDtoRepository = currencyConversionDtoRepository;
         this.restTemplateService = restTemplateService;
     }
 
+    private final CurrencyConversionDtoRepository currencyConversionDtoRepository;
+    private final RestTemplateService restTemplateService;
+
+
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCurrencyConversionDtoRequest")
     @ResponsePayload
     public GetCurrencyConversionDtoResponse getCurrencyConversionDto(@RequestPayload GetCurrencyConversionDtoRequest request) {
         GetCurrencyConversionDtoResponse response = new GetCurrencyConversionDtoResponse();
-        CurrencyConversionDto currencyConversionDto = currencyConversionDtoRepository.findCurrencyConversionDto(request.getFrom());
+
+        CurrencyConversionDto currencyConversionDto = request.getCurrencyConversionDto();
 
         CurrencyConversionDto currencyConversionDtoRest = restTemplateService.getCurrencyConversion(currencyConversionDto);
 
         response.setCurrencyConversionDto(currencyConversionDtoRest);
-
+//
         return response;
     }
 }
